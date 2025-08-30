7519f82c42c1c5a77674d57ae64fe32d262a351f
Joseph Liu <drowsy.liu@gmail.com>
2025-08-30 10:55:57 +0800

    remove reset playground in root

--- server/apps/playground/admin.py
+++ server/apps/playground/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from server.apps.playground.models import Item
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_active")
+    list_filter = ("is_active",)
