c0b85fe9180ee8070c87039ff721eabac28e4859
Joseph Liu <drowsy.liu@gmail.com>
2025-09-06 10:56:15 +0800

    use viewset

--- server/apps/playground/urls.py
+++ server/apps/playground/urls.py
@@ -1,10 +1,21 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from server.apps.playground.views import HiView, ItemDetailView, ItemView, hello
+from server.apps.playground.views import (
+    HiView,
+    ItemDetailView,
+    ItemView,
+    ItemViewSet,
+    hello,
+)
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"items-v2", ItemViewSet, basename="item")
 
 urlpatterns = [
     path("hello", hello),
     path("hi", HiView.as_view()),
     path("items", ItemView.as_view()),
     path("items/<int:item_id>/", ItemDetailView.as_view()),
+    path("viewset/", include(router.urls)),
 ]
--- server/apps/playground/views.py
+++ server/apps/playground/views.py
@@ -1,8 +1,21 @@
 # from django.shortcuts import render
+from django.http import Http404
 from rest_framework.decorators import api_view
-from rest_framework.generics import GenericAPIView, ListCreateAPIView
+from rest_framework.generics import (
+    GenericAPIView,
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from rest_framework.mixins import (
+    CreateModelMixin,  # 負責處理建立 (POST)
+    DestroyModelMixin,  # 負責處理刪除 (DELETE)
+    ListModelMixin,  # 負責列表 (GET)
+    RetrieveModelMixin,  # 負責單一物件存取 (GET)
+    UpdateModelMixin,  # 負責更新 (PUT, PATCH)
+)
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 from server.apps.management.serializer import ItemSerializer
 from server.apps.playground.models import Item
@@ -62,37 +75,69 @@ class ItemListView(ListCreateAPIView):
     queryset = Item.objects.all()
 
 
-class ItemDetailView(GenericAPIView):
+# GET /xxxxxx/items/<id> => 得到資料庫中指定的 Items
+
+## 版本一
+# class ItemDetailView(
+#     RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin, GenericAPIView
+# ):
+#     serializer_class = ItemSerializer
+#     queryset = Item.objects.all()
+#     lookup_url_kwarg = "item_id"
+
+#     def get(self, request, item_id):
+#         return self.retrieve(request, item_id)
+
+#     def delete(self, request, item_id):
+#         return self.destroy(request, item_id)
+
+#     def put(self, request, item_id):
+#         return self.update(request, item_id)
+
+#     def patch(self, request, item_id):
+#         return self.partial_update(request, item_id)
+
+
+## 版本二
+class ItemDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = ItemSerializer
+    queryset = Item.objects.all()
+    lookup_url_kwarg = "item_id"
 
-    def get_item(self, item_id):
-        try:
-            return Item.objects.get(id=item_id)
-        except Item.DoesNotExist:
-            return Response({"error": "Item not found"}, status=404)
 
-    def get(self, request, item_id):
-        item = self.get_item(item_id)
-        serializer = self.get_serializer(item)
-        return Response(serializer.data)
+## ViewSet
+class ItemViewSet(ModelViewSet):
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
 
-    def delete(self, request, item_id):
-        item = self.get_item(item_id)
-        item.delete()
-        return Response({"message": "Item deleted"}, status=204)
 
-    def put(self, request, item_id):
-        item = self.get_item(item_id)
-        serializer = self.get_serializer(item, data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=400)
-        serializer.save()
-        return Response(serializer.data)
+# class ItemDetailView(GenericAPIView):
+#     serializer_class = ItemSerializer
+#     queryset = Item.objects.all()
+#     lookup_url_kwarg = "item_id"
 
-    def patch(self, request, item_id):
-        item = self.get_item(item_id)
-        serializer = self.get_serializer(item, data=request.data, partial=True)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=400)
-        serializer.save()
-        return Response(serializer.data)
+# def get(self, request, item_id):
+#     item = self.get_object()
+#     serializer = self.get_serializer(item)
+#     return Response(serializer.data)
+
+# def delete(self, request, item_id):
+#     item = self.get_object()
+#     item.delete()
+#     return Response({"message": "Item deleted"}, status=204)
+
+# def put(self, request, item_id):
+#     item = self.get_object()
+#     serializer = self.get_serializer(item, data=request.data)
+#     if not serializer.is_valid():
+#         return Response(serializer.errors, status=400)
+#     serializer.save()
+#     return Response(serializer.data)
+
+# def patch(self, request, item_id):
+#     item = self.get_object()
+#     serializer = self.get_serializer(item, data=request.data, partial=True)
+#     if not serializer.is_valid():
+#         return Response(serializer.errors, status=400)
+#     serializer.save()
+#     return Response(serializer.data)
